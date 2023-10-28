3c2d2f6a96e35eec2cd54b635eead725830d04ca
bundithou <bundit.hou@gmail.com>
2023-10-29 02:37:07 +0700

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,5 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
   base: '/bizet/'
 })
